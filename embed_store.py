627ac9fe1e604e6e49d1b2bf6ae54b98e729a207
Vanjul Rander <127386212+VRM20@users.noreply.github.com>
2025-08-03 20:20:07 +0530

    Delete embed_store.py

--- embed_store.py
+++
@@ -1,9 +0,0 @@
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-
-def store_chunks_in_chroma(chunks):
-    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
-    vectordb = Chroma.from_texts(chunks, embeddings, persist_directory="chroma_db")
-    vectordb.persist()
-
-    return vectordb
