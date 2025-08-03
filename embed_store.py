fd93639dfa69799c8ab6cf72310b18222232ce5b
Vanjul Rander <127386212+VRM20@users.noreply.github.com>
2025-08-03 17:55:22 +0530

    Update embed_store.py

--- embed_store.py
+++ embed_store.py
@@ -1,10 +1,9 @@
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
 def store_chunks_in_chroma(chunks):
-    embeddings = HuggingFaceEmbeddings(model_name = "all-MiniLM-L6-v2")
+    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
     vectordb = Chroma.from_texts(chunks, embeddings, persist_directory="chroma_db")
     vectordb.persist()
 
     return vectordb
-
